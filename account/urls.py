4f443ef7cdcf127c6a342c26c92f2678edb22ca5
Samantha Trippy <srtrippy@gmail.com>
2017-11-09 00:38:42 -0600

    Added user registration, user profiles, and updated user dashboard.

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
 	#url(r'^login/$', views.user_login, name='login'),
 	url(r'^$', views.dashboard, name='dashboard'),
-	
+	url(r'^register/$', views.register, name='register'),
+	url(r'^edit/$', views.edit, name='edit'),
+
 	#login / logout views
 	url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
 	url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
